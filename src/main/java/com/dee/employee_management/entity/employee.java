28f3477de41a051609f6f1a7860a897d4d1fc38f
hoseaelsafan <hoseaelsafan@gmail.com>
2025-07-21 22:01:52 +0700

    Update user and pass from plan text to global variable from OS

--- src/main/java/com/dee/employee_management/entity/employee.java
+++ src/main/java/com/dee/employee_management/entity/employee.java
@@ -6,8 +6,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class employee {
 
     @Id
@@ -19,36 +25,4 @@ public class employee {
 
     private String department;
     private String email;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
