0032724fe9339b8a45484efee7d08fb073c5e0eb
hoseaelsafan <hoseaelsafan@gmail.com>
2025-08-06 22:22:16 +0700

    add to controller class, new pathfor Delete,update. change call class from 'just service' to Serviceinterface

--- src/main/java/com/dee/employee_management/controller/EmployeeController.java
+++ src/main/java/com/dee/employee_management/controller/EmployeeController.java
@@ -3,29 +3,42 @@ package com.dee.employee_management.controller;
 import com.dee.employee_management.dto.EmployeeManagementResponse;
 import com.dee.employee_management.dto.registerEmployeeRequest;
 import com.dee.employee_management.entity.employee;
-import com.dee.employee_management.service.Employeeservice;
+import com.dee.employee_management.service.EmployeeServiceInterface;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
-    private final Employeeservice employeeService;
+    private final EmployeeServiceInterface employeeServiceInterface;
 
-    public EmployeeController(Employeeservice employeeService) {
-        this.employeeService = employeeService;
+    public EmployeeController(EmployeeServiceInterface employeeServiceInterface) {
+        this.employeeServiceInterface = employeeServiceInterface;
     }
 
     @PostMapping("/register")
     public ResponseEntity<EmployeeManagementResponse<employee>> register(@RequestBody registerEmployeeRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(employeeService.saveEmployee(request));
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeServiceInterface.saveEmployee(request));
     }
 
     @GetMapping
     public ResponseEntity<EmployeeManagementResponse<List<employee>>> getAll() {
-        return ResponseEntity.ok(employeeService.getAllEmployees());
+        return ResponseEntity.ok(employeeServiceInterface.getAllEmployees());
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeManagementResponse<employee>> updateById(@RequestBody registerEmployeeRequest request,@PathVariable Long id){
+        EmployeeManagementResponse<employee> response = employeeServiceInterface.updateEmployee(request, id);
+        return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<EmployeeManagementResponse<String>> delete(@PathVariable Long id){
+        return ResponseEntity.ok(employeeServiceInterface.deleteEmployee(id));
+    }
+
 }
