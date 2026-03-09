e25c795d32e490101227335872008678077be852
hoseaelsafan <hoseaelsafan@gmail.com>
2026-03-09 21:56:30 +0700

    modified dto to proper for JUnit

--- src/main/java/com/dee/employee_management/dto/EmployeeManagementResponse.java
+++ src/main/java/com/dee/employee_management/dto/EmployeeManagementResponse.java
@@ -3,10 +3,12 @@ package com.dee.employee_management.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class EmployeeManagementResponse<T> {
     private String responseCode;
     private String message;
--- src/main/java/com/dee/employee_management/dto/registerEmployeeRequest.java
+++ src/main/java/com/dee/employee_management/dto/registerEmployeeRequest.java
@@ -5,9 +5,11 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class registerEmployeeRequest {
     @NotBlank(message = "Name is required")
     private String name;
